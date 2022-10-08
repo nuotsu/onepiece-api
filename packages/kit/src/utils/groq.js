64e99218dde0783abcddf6b2ae01f8157d0a2261
Mitchell Christ <xzmaru@gmail.com>
2022-10-07 19:01:52 -0700

    hide drafts

--- packages/kit/src/utils/groq.js
+++ packages/kit/src/utils/groq.js
@@ -1,5 +1,7 @@
 import groq from 'groq'
 
+export const live = groq`!(_id in path('drafts.*'))`
+
 export const quote_count = groq`
 	count(*[_type=='chapter'].quotes[].saidBy[references(^._id)])
 `
