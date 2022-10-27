c1053360f144db7452027e693ae3003659129414
Mitchell Christ <xzmaru@gmail.com>
2022-10-26 20:53:41 -0700

    add randomizer button

--- packages/kit/src/routes/index/RandomQuote.svelte
+++ packages/kit/src/routes/index/RandomQuote.svelte
@@ -2,9 +2,30 @@
 	<h2 class="heading -mb-2 text-center">Random Quote</h2>
 
 	<div class="bordered p-2">
-		<Quote {chapter} {...quote} />
+		{#key random}
+			<Quote {...random} />
+		{/key}
 	</div>
+
+	<p class="text-right text-xs">
+		<button class="text-ink/30 link" on:click={() => { random = getRandom() }}>
+			Randomize
+		</button>
+	</p>
 </section>
+
+<style>
+	div > :global(*) {
+		animation: appear 0.2s ease-in-out forwards;
+	}
+
+	@keyframes appear {
+		from {
+			opacity: 0;
+			translate: -1rem 0;
+		}
+	}
+</style>
 
 <script>
 	import Quote from '$lib/quote/Quote.svelte'
@@ -13,13 +34,22 @@
 
 	const { chapters, global } = $page.data
 
-	$: resolveSpoilers = [...chapters]
-		.reverse()
-		.slice(0, $spoilers ? chapters.map(c => c.number).indexOf(global.spoiler) + 1 : chapters.length)
+	let random = getRandom()
 
-	$: ({ quotes, ...chapter } = resolveSpoilers[
-		Math.floor(Math.random() * resolveSpoilers.length)
-	])
+	function getRandom() {
+		const resolveSpoilers = [...chapters]
+			.reverse()
+			.slice(0, $spoilers ? chapters.map(c => c.number).indexOf(global.spoiler) + 1 : chapters.length)
 
-	$: quote = quotes[Math.floor(Math.random() * quotes.length)]
+		const { quotes, ...chapter } = resolveSpoilers[
+			Math.floor(Math.random() * resolveSpoilers.length)
+		]
+
+		const quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+		return {
+			chapter,
+			...quote
+		}
+	}
 </script>
